b10bf9fc28acabdc6bca93d311ffdc38752a70b6
Monica <monica@192.168.1.8>
2026-04-05 20:23:55 +0700

    increase body limit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json({ limit: "100kb" }));
+app.use(express.json({ limit: "10mb" }));
 
 const GEMINI_API_KEY = "AIzaSyCD1wi80mscT8pO8DYuzdfZSgufsP5hjbw";
 
